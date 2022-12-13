dafd224285107bf33b82a0d8be6329c5305f97d5
Humaira Tasnim <hxtasnim@gmail.com>
2022-12-13 14:19:36 -0500

    refactored and created input module

--- client.js
+++ client.js
@@ -22,6 +22,4 @@ const connect = function () {
   return conn;
 };
 
-module.exports = {
-  connect
-};
+module.exports = { connect };
--- play.js
+++ play.js
@@ -1,24 +1,7 @@
-const { connect } = require("./client.js");
+const { connect } = require("./client");
+const { setupInput } = require("./input");
 
 console.log("Connecting ...");
 connect();
-
-// setup interface to handle user input from stdin
-
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on("data", handleUserInput);
-  return stdin;
-};
-
-const handleUserInput = function (data) {
-  // \u0003 maps to ctrl+c input
-  if (data === '\u0003') {
-    process.exit();
-  }
-};
 
 setupInput();
