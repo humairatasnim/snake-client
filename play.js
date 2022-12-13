9a1bbd54fb69877697c5b3cfcf2c5b11bee72b28
Humaira Tasnim <hxtasnim@gmail.com>
2022-12-13 14:13:18 -0500

    added setupInput and handleUserInput functions

--- play.js
+++ play.js
@@ -2,3 +2,23 @@ const { connect } = require("./client.js");
 
 console.log("Connecting ...");
 connect();
+
+// setup interface to handle user input from stdin
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+
+const handleUserInput = function (data) {
+  // \u0003 maps to ctrl+c input
+  if (data === '\u0003') {
+    process.exit();
+  }
+};
+
+setupInput();
